2b830887e183af85ec6598df9ff88ab346dda99e
vankhuon91 <vankhuon91@gmail.com>
2024-01-14 19:00:02 +0700

    done socket

--- go.mod
+++ go.mod
@@ -12,6 +12,7 @@ require (
 	github.com/google/uuid v1.5.0 // indirect
 	github.com/googollee/go-socket.io v1.7.0 // indirect
 	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/joho/godotenv v1.5.1
 	github.com/klauspost/compress v1.17.4 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
--- main.go
+++ main.go
@@ -1,62 +1,94 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"os"
 
 	socketio "github.com/googollee/go-socket.io"
+	_ "github.com/joho/godotenv/autoload"
 )
+
+type AttrJson map[string]interface{}
+type Connections map[string]*socketio.Conn
+
+type Msg struct {
+	From        string   `json:"from"`
+	To          string   `json:"to"`
+	CommandType string   `json:"command_type"`
+	CommandInfo AttrJson `json:"command_info"`
+}
+
+var UserConns Connections
+var AgentConns Connections
 
 func main() {
 	server := socketio.NewServer(nil)
+	UserConns = make(Connections)
+	AgentConns = make(Connections)
 
-	server.OnConnect("/admin", func(s socketio.Conn) error {
-		s.SetContext("")
-		s.Emit("reply", "connected")
-		fmt.Println("connected:", s.ID())
+	//user connect
+	server.OnConnect("/user", func(s socketio.Conn) error {
+		token := s.RemoteHeader().Get("token")
+		client := s.RemoteHeader().Get("client")
+		if (token == "") || (client == "") {
+			s.Close()
+			return nil
+		}
+		UserConns[client] = &s
+
+		//s.SetContext(client)
+		log.Println("User connected:", s.ID(), token, client)
+
 		return nil
 	})
 
+	//agent connect
 	server.OnConnect("/agent", func(s socketio.Conn) error {
-		s.SetContext("")
-		s.Emit("reply", "connected")
-		fmt.Println("connected:", s.ID())
+		token := s.RemoteHeader().Get("token")
+		client := s.RemoteHeader().Get("client")
+		if (token == "") || (client == "") {
+			s.Close()
+			return nil
+		}
+		AgentConns[client] = &s
+		//s.SetContext(client)
+		log.Println("Agent connected:", s.ID(), token, client)
 		return nil
 	})
 
-	server.OnEvent("/", "notice", func(s socketio.Conn, msg string) {
-		fmt.Println("notice:", msg)
+	//user send message
+	server.OnEvent("/user", "msg", func(s socketio.Conn, msg Msg) {
+		log.Println("msg from user:", msg)
+		var AgentConn socketio.Conn
+		if AgentConns[msg.To] != nil {
+			msg.From = s.RemoteHeader().Get("client")
+			AgentConn = *AgentConns[msg.To]
+			AgentConn.Emit("msg", msg)
+			log.Println("send to", msg.To)
+		}
 
-		s.Emit("reply", "have "+msg)
 	})
 
-	server.OnEvent("/chat", "msg", func(s socketio.Conn, msg string) string {
-		s.SetContext(msg)
-		return "recv " + msg
-	})
+	//agent send message
+	server.OnEvent("/agent", "msg", func(s socketio.Conn, msg Msg) {
+		log.Println("msg from agent:", msg)
+		var UserConn socketio.Conn
+		if UserConns[msg.To] != nil {
+			msg.From = s.RemoteHeader().Get("client")
+			UserConn = *UserConns[msg.To]
+			UserConn.Emit("msg", msg)
+			log.Println("send to", msg.To)
+		}
 
-	server.OnEvent("/", "bye", func(s socketio.Conn) string {
-		last := s.Context().(string)
-		s.Emit("bye", last)
-		s.Close()
-		return last
-	})
-
-	server.OnError("/", func(s socketio.Conn, e error) {
-		// server.Remove(s.ID())
-		fmt.Println("meet error:", e)
 	})
 
 	server.OnDisconnect("/", func(s socketio.Conn, reason string) {
-		// Add the Remove session id. Fixed the connection & mem leak
-		//server.Remove(s.ID())
-		fmt.Println("closed", reason)
+		log.Println("closed:", s.Context(), reason)
 	})
 
 	go server.Serve()
-	//defer server.Close()
+	defer server.Close()
 
 	http.Handle("/socket.io/", server)
 	port := os.Getenv("PORT")
